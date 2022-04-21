25a68f972289f67590b3592b794fbd2eb9636068
CooperStansbury <cstansbu@umich.edu>
2022-04-21 14:04:49 -0400

    reorganize filters and utils; clean summaries

---
+++ notebooks/filters.py
@@ -0,0 +1,244 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+import sys
+
+
+def adjacentContactFilter(df):
+    """A filter to drop contacts that are products of clique expansion.
+    This assumes that only adjacent alignments on the read are considered
+    contacting.
+    
+    args:
+        : df (pd.DataFrame): the contact table
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering
+    """
+    mask = (df['contact_is_direct'] == True)
+    df = df[mask].reset_index(drop=True)
+    return df
+
+
+def selfLoopFilter(df):
+    """A filter to drop contacts between the same fragment at reda-level
+    
+    args:
+        : df (pd.DataFrame): the contact table
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering
+    """
+    mask = (df['align1_fragment_id'] == df['align2_fragment_id']) 
+    df = df[~mask].reset_index(drop=True)
+    return df
+
+
+def mapQFilter(df, lowerBound, upperBound):
+    """A filter to bandpass on mapping quality 
+    
+    args:
+        : df (pd.DataFrame): the contact table
+        : lowerBound (int): a lower bound on mapq, suggested 30
+        : upperBound (int): an upper bound, suggested 250
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering
+    """
+    mask = (df['align1_mapping_quality'] >= lowerBound) & (df['align2_mapping_quality'] >= lowerBound)
+    df = df[mask].reset_index(drop=True)
+    
+    mask = (df['align1_mapping_quality'] <= upperBound) & (df['align2_mapping_quality'] <= upperBound)
+    df = df[mask].reset_index(drop=True)
+    
+    return df
+
+
+def distalContactFilter(df, read_distance=1000):
+    """A filter to remove direct contacts that are separated by 
+    long unmapped sequences on the read.
+    
+    NOTE: this filter is not applied to clique expansion products
+
+    args:
+        : df (pd.DataFrame): the contact table
+        : read_distance (int): minimum base pair distance between
+        direct contacts. 
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering
+    """
+    mask = (df['contact_is_direct'] == True)
+    
+    directContact = df[mask].reset_index(drop=True)
+    expandedContact = df[~mask].reset_index(drop=True)
+    
+    mask = (np.abs(directContact['contact_read_distance']) <= read_distance) 
+    directContact = directContact[mask].reset_index(drop=True)
+    
+    df = pd.concat([directContact, expandedContact])
+    
+    return df
+
+
+def closeContactFilter(df, genome_distance=1000):
+    """A filter to remove cis contacts within a small distance on
+    the reference. Assumed to be amplicification bias.
+    
+    args:
+        : df (pd.DataFrame): the contact table
+        : genome_distance (int): maximum base pair distance between
+        aligned fragments. 
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering 
+    """
+    cisContacts = df[df['contact_is_cis'] == True]
+    transContacts = df[df['contact_is_cis'] == False]
+    
+    mask = (np.abs(cisContacts['contact_genome_distance']) >= genome_distance)
+    cisContacts = cisContacts[mask].reset_index(drop=True)
+
+    df = pd.concat([cisContacts, transContacts], ignore_index=True)
+    return df
+
+
+def duplicateContactFilter(df, retain=1):
+    """A filter to retain only n copies of a given contact. 
+    Note that `retain = 1` will filter to unique conacts only. 
+    Contacts are retained by mean mapping quality, such that the 
+    highest n mean mapping quality contacts are preserved.
+    
+    args:
+        : df (pd.DataFrame): the contact table
+        : retain (int): number of replicates to preserve
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering 
+    """
+    df['mean_mapping_quality'] = (df['align1_mapping_quality'] + df['align2_mapping_quality']) / 2
+    
+    df = df.sort_values(by=['align1_fragment_id', 
+                            'align2_fragment_id', 
+                            'mean_mapping_quality'], ascending=False)
+    
+    df['contact_count'] = df.groupby(["align1_fragment_id", "align2_fragment_id"])["read_name"].transform("cumcount")
+    df['contact_count'] = df['contact_count'] + 1
+
+    mask = (df['contact_count'] <= retain)
+    df = df[mask].reset_index(drop=True)
+    return df
+
+
+def getFragmentCounts(df):
+    """A helper function to return fragment counts
+    from both sides of a contact """
+    allFragments = df['align1_fragment_id'].tolist() + df['align2_fragment_id'].tolist()
+    return Counter(allFragments)
+    
+
+def ligationProductFilter(df, nProducts=4, nReadReplicates=4, verbose=True):
+    """A filter to remove excess copies of an aligned sequence. A single
+    cell can have at most 4 copies of any genomic sequence (maternal, paternal)
+    2x if the cell is about to divide. Two step filter:
+        (1) separate contacts with fewer than `nProducts` and more than
+        `nProducts`. Those with more than `nProducts` are considered amplification 
+        bias.
+        
+        (2) For sequences with amplificiation bias, retain only contacts
+        between sequences that appear on fewer than `nReadReplicates`. 
+        Sequences found on more reads will be filtered out.
+    
+    args:
+        : df (pd.DataFrame): the contact table
+        : nProducts (int): number of products for the initial filter
+        : nReadReplicates (int): number of reads an amplified sequence
+        may be found on
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering 
+    """
+    # get fragment counts
+    countTranslation = dict(getFragmentCounts(df))
+
+    df['align1_fragcount'] = df['align1_fragment_id'].map(countTranslation)
+    df['align2_fragcount'] = df['align2_fragment_id'].map(countTranslation)
+
+    mask = (df['align1_fragcount'] <= nProducts) & (df['align2_fragcount'] <= nProducts)
+
+    # divide into contacts with less than n replicates and more than n replicates
+    lowFrequencyProducts = df[mask].reset_index(drop=True)
+    amplificationBiasProducts = df[~mask].reset_index(drop=True)
+    
+    # count the number of unique reads per fragment
+    readsPerFragment1 = amplificationBiasProducts[['read_name', 'align1_fragment_id']]
+    readsPerFragment2 = amplificationBiasProducts[['read_name', 'align2_fragment_id']]
+    
+    readsPerFragment1.columns = ['read_name', 'fragment_id']
+    readsPerFragment2.columns = ['read_name', 'fragment_id']
+
+    readsPerFragment = pd.concat([readsPerFragment1, readsPerFragment2], ignore_index=True)
+    
+    if verbose:
+        print(f"{lowFrequencyProducts.shape=} ({len(lowFrequencyProducts)/len(df):.3f})")
+        print(f"{amplificationBiasProducts.shape=} ({len(amplificationBiasProducts)/len(df):.3f})")
+
+        print(f"{readsPerFragment1.shape=}")
+        print(f"{readsPerFragment2.shape=}")
+        print(f"{readsPerFragment.shape=}")
+    
+    readsPerFragment['n_reads'] = readsPerFragment.groupby('fragment_id')["read_name"].transform('nunique')
+    readsPerFragmentMappable = pd.Series(readsPerFragment['n_reads'].values, index=readsPerFragment['fragment_id']).to_dict()
+    
+    # map the number of reads an individual fragment appears in
+    amplificationBiasProducts['align1_n_reads'] = amplificationBiasProducts['align1_fragment_id'].map(readsPerFragmentMappable)
+    amplificationBiasProducts['align2_n_reads'] = amplificationBiasProducts['align2_fragment_id'].map(readsPerFragmentMappable)
+    
+    # throw away all contacts which appear in more than n reads 
+    mask = (amplificationBiasProducts['align1_n_reads'] <= nReadReplicates) & (amplificationBiasProducts['align2_n_reads'] <= nReadReplicates)
+    
+    toKeep = amplificationBiasProducts[mask].reset_index(drop=True)
+    trash = amplificationBiasProducts[~mask].reset_index(drop=True)
+
+    if verbose:
+            print(f"{toKeep.shape=}")
+            print(f"{trash.shape=}")
+    
+    df = pd.concat([lowFrequencyProducts, toKeep])
+    
+    return df
+
+
+def chromosomalFilter(df, assembly):
+    """A filter to remove non-chromosomal genomic regions
+    
+    args:
+        : df (pd.DataFrame): the contact table
+        : assembly (pd.DataFrame): assembly information
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering 
+    """
+    allChromosomes = assembly['RefSeq accession'].str.strip().to_list()
+    
+    mask = (df['align1_chrom'].isin(allChromosomes)) & (df['align2_chrom'].isin(allChromosomes))
+    df = df[mask].reset_index()
+    return df
+
+
+def removeYChrom(df):
+    """A filter to remove Y chromosome artefacts
+    
+    args:
+        : df (pd.DataFrame): the contact table
+    
+    returns:
+        : df (pd.DataFrame): the contact table after filtering 
+    """
+    mask = (df['align1_chrom'] == 'NC_000087.8') | (df['align2_chrom'] == 'NC_000087.8')
+    df = df[~mask].reset_index(drop=True)
+    return df
---
+++ notebooks/pore_c_utils.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import numpy as np
+from collections import Counter
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import os
+import sys
+
+
+
+def loadContactTable(directory, filetag):
+    """A function to load and merge a set of contact tables
+    from a directory based on keyword filename matching
+    
+    args:
+        : directory (str): path including tailing backslash
+        : filetag (str): the matching keyword used to load all
+            contact tables for a given object
+    
+    returns:
+        : df (pd.DataFrame): a dataframe of the Nanopore Pore-C
+        contact table
+    """
+    df_list = []
+    for f in os.listdir(directory):
+        if filetag in f:
+            filepath = f"{directory}{f}"
+            tmp = pd.read_parquet(filepath)
+            tmp['batch_id'] = f.split("_")[2]
+            df_list.append(tmp)
+
+    df = pd.concat(df_list)
+    return df
+
+
+def getFragmentMAPQ(df):
+    """A function to return the mapq scores of all fragments
+    
+    returns:
+        : mapq (pd.DataFrame): a two-column dataframe with fragment
+        ids and mapping quality
+    """
+    align1 = df[['align1_fragment_id', 'align1_mapping_quality']]
+    align1.columns = ['fragment_id', 'mapq']
+
+    align2 = df[['align2_fragment_id', 'align2_mapping_quality']]
+    align2.columns = ['fragment_id', 'mapq']
+
+    mapq = pd.concat([align1, align2], ignore_index=True)
+    return mapq
+
+
+def intervsIntraSummary(df):
+    """A function to print summary statistics regarding the ratio
+    of inter and intra chromosomal contacts
+    
+    returns:
+        : res (pd.DataFrame): a summary table
+    """
+    totalContacts = len(df)
+    
+    isIntra = np.where(df['align1_chrom'] == df['align2_chrom'], 1, 0)
+    nIntra = np.sum(isIntra)
+    nInter = totalContacts - nIntra
+    
+    metrics = ['Total Contacts', 'Intra- Contacts', 'Inter- Contacts']
+    values = [totalContacts, nIntra, nInter]
+    
+    res = {
+        'Metric' : metrics,
+        'Value' : values,
+        'Percentage' : [x/totalContacts for x in values]
+    }
+    res = pd.DataFrame(data=res)
+    return res
+    
+    
+def cisTransSummary(df):
+    """A function to print the cis-trans ratio as defined by Nanopore
+    Pore-C pipeline 
+    
+    returns:
+        : res (pd.DataFrame): a summary table
+    """
+    
+    totalContacts = len(df)
+    cisContacts = np.sum(np.where(df['contact_is_cis'] == 1, 1, 0))
+    transContacts = np.sum(np.where(df['contact_is_cis'] == 0, 1, 0))
+    cisTransRatio = cisContacts/transContacts
+    
+    metrics = ['Total Contacts', 'cis Contacts', 'trans Contacts', 'Cis/Trans Ratio']
+    values = [totalContacts, cisContacts, transContacts, cisTransRatio]
+    perc = [x/totalContacts for x in values]
+    perc[-1] = 0
+    
+    res = {
+        'Metric' : metrics,
+        'Value' : values,
+        'Percentage' : perc
+    }
+    res = pd.DataFrame(data=res)
+    return res
+    
+
+def contactDirectSummary(df):
+    """A function to count the number of direct vs.
+    indirect contacts. Indirect contacts are clique expansion 
+    products
+    
+    returns:
+        : res (pd.DataFrame): a summary table
+    """
+    totalContacts = len(df)
+    
+    isDirect = np.sum(np.where(df['contact_is_direct'] == 1, 1, 0))
+    inDirect = totalContacts - isDirect
+    
+    metrics = ['Total Contacts', 'Direct Contacts', 'Indirect Contacts']
+    values = [totalContacts, isDirect, inDirect]
+    
+    res = {
+        'Metric' : metrics,
+        'Value' : values,
+        'Percentage' : [x/totalContacts for x in values]
+    }
+    res = pd.DataFrame(data=res)
+    return res
+    
+    
+def contactOrderSummary(df):
+    """A function to print higher order contact summary
+    
+    returns:
+        : res (pd.DataFrame): a summary table
+    """
+    concatemerCounts = df.groupby('read_name')['align1_fragment_id'].count().reset_index()
+    concatemerCounts.columns = ['read_name', 'count']
+    
+    nReads = len(concatemerCounts)
+    
+    nSingleton = np.sum(np.where(concatemerCounts['count'] == 1, 1, 0))
+    nPair = np.sum(np.where(concatemerCounts['count'] == 2, 1, 0))
+    nMultiway = np.sum(np.where(concatemerCounts['count'] > 2, 1, 0))
+
+    metrics = ['Total Reads', 'Singletons', 'Pairs', 'Multiway']
+    values = [nReads, nSingleton, nPair, nMultiway]
+    
+    res = {
+        'Metric' : metrics,
+        'Value' : values,
+        'Percentage' : [x/nReads for x in values]
+    }
+    res = pd.DataFrame(data=res)
+    return res
+    
+    
+def getSummary(df):
+    """A function to gather summary information from the current dataframe"""
+    
+    empty_row = {
+        'Metric' : ['--'],
+        'Value' : ['--'],
+        'Percentage' : ['--']
+    }
+    
+    empty = pd.DataFrame(data=empty_row)
+    
+    res = pd.concat([contactOrderSummary(df), 
+                     empty,
+                     cisTransSummary(df), 
+                     empty,
+                     intervsIntraSummary(df),
+                     empty,
+                     contactDirectSummary(df)]
+                     ).reset_index()
+    return res
+    
+    
+def printSummary(res):
+    """A function to print the summary compactly"""
+    
+    for idx, row in res.iterrows():
+        
+        if isinstance(row['Percentage'], float):
+            print(f"{row['Metric']} {row['Value']} ({row['Percentage']:.3f})")
+        else:
+            print(f"{row['Metric']} {row['Value']} {row['Percentage']}")
